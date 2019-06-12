7e29960e5387d187cda90d0cf1ba9a8ff3083d9e
madhuri <51332677+mdhuri@users.noreply.github.com>
2019-06-12 18:17:34 +0530

    Add files via upload

---
+++ motorpotentiometer.c
@@ -0,0 +1,87 @@
+/*
+ * motorwithpotentiometer.c
+ *
+ * Created: 11-06-2019 12:18:32
+ * Author : Madhu
+ */ 
+
+#define F_CPU 1000000
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#define enable 5
+#define registerselection 7
+void send_a_command(unsigned char command);
+void send_a_character(unsigned char character);
+void send_a_string(char *string_of_character);
+void PWM_init()
+{
+	TCCR0=(1<<WGM00)|(1<<WGM01)|(1<<COM01)|(1<<CS00);
+	DDRB|=(1<<PB3);
+}
+
+int main(void)
+{
+	TCCR0=(1<<WGM00)|(1<<WGM01)|(1<<COM01)|(1<<CS00);
+	DDRB|=(1<<PB3);
+	DDRB|=(1<<PB2);
+	DDRC=0XFF;
+	DDRA=0X00;
+	DDRD=0XFF;
+	
+	_delay_ms(50);
+	ADMUX|=(1<<REFS0)|(1<<REFS1);
+	ADCSRA|=(1<<ADEN)|(1<<ADATE)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2);
+	int count=0;
+	char showa[3];
+	send_a_command(0X01);
+	_delay_ms(50);
+	send_a_command(0x38);
+	_delay_ms(50);
+	send_a_command(0x0E);
+	_delay_ms(50);
+	ADCSRA|=(1<<ADSC);
+	PWM_init();
+	while (1)
+	
+	{
+		count=ADC/4;
+		OCR0=count;
+		send_a_command(0X80+0X40+0);
+		send_a_string("Temp(C)= ");
+		send_a_command(0X80+0X40+8);
+		
+		itoa(count,showa,10);
+		send_a_string(showa);
+		send_a_string("  ");
+		send_a_command(0X80+0);
+	}
+}
+void send_a_command( unsigned char command)
+{
+	PORTC=command;
+	PORTD&=~(1<<registerselection);
+	PORTD|=(1<<enable);
+	_delay_ms(50);
+	PORTD&=~(1<<enable);
+	PORTC=0;
+}
+void send_a_character(unsigned char character)
+{
+	PORTC=character;
+	PORTD|=(1<<registerselection);
+	PORTD|=(1<<enable);
+	_delay_ms(50);
+	PORTD&=~(1<<enable);
+	PORTC=0;
+}
+void send_a_string(char *string_of_character)
+{
+	while(*string_of_character>0)
+	{
+		send_a_character(*string_of_character++);
+	}
+}
+
+
+
