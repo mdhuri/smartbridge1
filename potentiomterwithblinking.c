c6ce13b753804ab932cfe463013b6c79c0a71838
madhuri <51332677+mdhuri@users.noreply.github.com>
2019-06-12 18:19:17 +0530

    Add files via upload

---
+++ potentiomterwithblinking.c
@@ -0,0 +1,18 @@
+/*
+ * poteniometerwithblinking.c
+ *
+ * Created: 10-06-2019 14:42:40
+ * Author : Madhu
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    
+    while (1) 
+    {
+    }
+}
+
