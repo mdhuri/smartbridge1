1fb89ee30a1834369f644eb29e8c8fcab42b58d9
madhuri <51332677+mdhuri@users.noreply.github.com>
2019-06-12 18:32:41 +0530

    Add files via upload

---
+++ switchlcd.c
@@ -0,0 +1,18 @@
+/*
+ * switch lcd.c
+ *
+ * Created: 08-06-2019 14:42:20
+ * Author : Madhu
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
